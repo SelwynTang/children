63b51a3105a4d40dbdbc290b56d66023ecc2c628
Selwyn <tangh@chainen.net>
2018-06-04 10:35:46 +0800

    首次提交

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var $width = $(document).width();
+    var $height = $(document).height();
+    $(".mask").width($width); 
+    $(".mask").height($height);
+
+    function showMask(){     
+          $(".mask").show();     
+      }  
+      //隐藏遮罩层  
+      function hideMask(){   
+          $(".mask").hide();    
+      } 
+
+      $(".btn").click(function(){
+        showMask()
+      })
+      $(".btn01").click(function(){
+        hideMask()
+      })
+})
